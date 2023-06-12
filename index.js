ecfb0b1127c135f56391afcdbdef7e965457a3ae
Dipto Das <ddipto396@gmail.com>
2023-06-12 10:51:09 +0600

    server hosted in vervel

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const userCollection = client.db('rhythm').collection('userInfo');
     const classCollection = client.db('rhythm').collection('classInfo');
